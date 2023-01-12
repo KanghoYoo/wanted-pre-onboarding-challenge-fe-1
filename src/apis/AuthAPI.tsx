3bb87e5f834e472bd0b0caa02b0c1ab1dacc8f76
KanghoYoo <kangsick98@gmail.com>
2023-01-12 20:19:29 +0900

    Modify: 코드 간단화

--- src/apis/AuthAPI.tsx
+++ src/apis/AuthAPI.tsx
@@ -4,12 +4,12 @@ const URI = "http://localhost:8080";
 
 export const signIn = (email: string, password: string) =>
   axios.post(`${URI}/users/login`, {
-    email: email,
-    password: password,
+    email,
+    password,
   });
 
 export const signUp = (email: string, password: string) =>
   axios.post(`${URI}/users/create`, {
-    email: email,
-    password: password,
+    email,
+    password,
   });
